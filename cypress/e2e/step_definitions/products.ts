56074d29dd12746e23d4ba209373a3df0e0b9ff7
Rachel DA SILVA SANTOS <rachel-da-silva-santos@samse.fr>
2024-03-04 21:53:01 +0100

    fix typo

--- cypress/e2e/step_definitions/products.ts
+++ cypress/e2e/step_definitions/products.ts
@@ -5,7 +5,7 @@ When("search product by key words {string}", (search : string) => {
     cy.get('#submit_search').click();
 });
 
-Then("add the product {string} to cart", (productName:string) => {
+When("add the product {string} to cart", (productName:string) => {
     cy.get('.product-image-wrapper').contains(productName)
         .parentsUntil('.product-image-wrapper')
         .find('.add-to-cart').first().click();
